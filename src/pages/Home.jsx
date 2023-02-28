ef02f4509a3f283a6e4b53e8b491ba936d439415
yoon <funfunjulia@gmail.com>
2023-02-28 14:31:31 +0900

    del row section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,7 +10,6 @@ function Home() {
       <Row rowID='1' title="TopRated" fetchURL={requests.requestTopRated}/>
       <Row rowID='2' title="Popular" fetchURL={requests.requestPopular}/>
       <Row rowID='3' title="Trending" fetchURL={requests.requestTrending}/>
-      <Row rowID='4' title="Horror" fetchURL={requests.requestHorror}/>
       <Row rowID='5' title="Upcoming" fetchURL={requests.requestUpcoming}/>
     </div>
   )
